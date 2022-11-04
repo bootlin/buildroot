c73e1f8d8f12f0ac8ecdeb97a01c2a24f8f1108c
Clément Léger <clement.leger@bootlin.com>
2022-11-04 15:19:37 +0100

    debugging: simpler locking.c module
    Signed-off-by: Clément Léger <clement.leger@bootlin.com>

--- board/stmicroelectronics/stm32mp157a-dk1-debugging/overlay/root/locking/locking.c
+++ board/stmicroelectronics/stm32mp157a-dk1-debugging/overlay/root/locking/locking.c
@@ -16,17 +16,10 @@
 #include <linux/mutex.h>
 
 static void my_work_fn(struct work_struct *work);
-static DECLARE_WORK(appldata_work, my_work_fn);
+static DECLARE_WORK(my_custom_work, my_work_fn);
 
 static DEFINE_MUTEX(mutex1);
 static DEFINE_MUTEX(mutex2);
-
-static void do_work(struct mutex *mutex)
-{
-	mutex_lock(mutex);
-	pr_info("Doing work !\n");
-	mutex_unlock(mutex);
-}
 
 static void do_atomic_work(void)
 {
@@ -45,16 +38,24 @@ static void do_atomic_work(void)
 static void my_work_fn(struct work_struct *work)
 {
 	mutex_lock(&mutex1);
-	do_work(&mutex2);
+	mutex_lock(&mutex2);
+
+	pr_info("Doing work !\n");
+
+	mutex_unlock(&mutex2);
 	mutex_unlock(&mutex1);
 }
 
 static int __init locking_init(void)
 {
-	schedule_work(&appldata_work);
+	schedule_work(&my_custom_work);
 
 	mutex_lock(&mutex2);
-	do_work(&mutex1);
+	mutex_lock(&mutex1);
+
+	pr_info("Doing some init work too !\n");
+
+	mutex_unlock(&mutex1);
 	mutex_unlock(&mutex2);
 
 	return 0;
