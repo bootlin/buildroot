c87d0af987e654210132ee8fbc033093c45ea634
Clément Léger <clement.leger@bootlin.com>
2022-11-09 13:40:11 +0100

    debugging: add sched_intensive program
    Signed-off-by: Clément Léger <clement.leger@bootlin.com>

--- package/debugging-training/debugging-training.mk
+++ package/debugging-training/debugging-training.mk
@@ -7,7 +7,13 @@
 DEBUGGING_TRAINING_VERSION = 1.0
 DEBUGGING_TRAINING_SITE = board/stmicroelectronics/stm32mp157a-dk1-debugging/source
 DEBUGGING_TRAINING_SITE_METHOD = local
-DEBUGGING_TRAINING_MODULE_SUBDIRS = irqs_latency/ kmemleak/ locking/
 
-$(eval $(kernel-module))
+define DEBUGGING_TRAINING_BUILD_CMDS
+	$(TARGET_CC) $(TARGET_CFLAGS) $(TARGET_LDFLAGS) $(@D)/sched_intensive/sched_intensive.c -o $(@D)/sched_intensive/sched_intensive
+endef
+
+define DEBUGGING_TRAINING_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/sched_intensive/sched_intensive $(TARGET_DIR)/usr/bin/sched_intensive
+endef
+
 $(eval $(generic-package))
