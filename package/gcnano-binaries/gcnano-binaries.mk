44dda5705a218f8a1b9818a8b9a482a628888581
Kory Maincent <kory.maincent@bootlin.com>
2022-11-23 10:06:46 +0100

    package/gcnano-binaries: bump version to 6.4.9
    Update to the latest version of the gcnano-binaries blobs, which are
    compatible with the last 5.15 kernel from the STMicroelectronics BSP.
    
    Signed-off-by: Kory Maincent <kory.maincent@bootlin.com>

--- package/gcnano-binaries/gcnano-binaries.mk
+++ package/gcnano-binaries/gcnano-binaries.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-GCNANO_BINARIES_LIB_VERSION = 6.4.7
+GCNANO_BINARIES_LIB_VERSION = 6.4.9
 GCNANO_BINARIES_DRIVER_VERSION = $(GCNANO_BINARIES_LIB_VERSION)
-GCNANO_BINARIES_USERLAND_VERSION = $(GCNANO_BINARIES_LIB_VERSION)-20220524
-GCNANO_BINARIES_VERSION = a20611434ef048d3d0c04f55b6cd7d75a2156d43
+GCNANO_BINARIES_USERLAND_VERSION = $(GCNANO_BINARIES_LIB_VERSION)-20221206
+GCNANO_BINARIES_VERSION = 0ac1a89d7a59d040a69745a85f0da7e98644cc4b
 GCNANO_BINARIES_SITE = $(call github,STMicroelectronics,gcnano-binaries,$(GCNANO_BINARIES_VERSION))
 
 GCNANO_BINARIES_LICENSE = MIT, Vivante End User Software License Terms
@@ -38,7 +38,7 @@ GCNANO_BINARIES_POST_EXTRACT_HOOKS += GCNANO_BINARIES_EXTRACT_HELPER
 
 GCNANO_BINARIES_MODULE_MAKE_OPTS = \
 	KERNEL_DIR=$(LINUX_DIR) \
-	SOC_PLATFORM=st-st \
+	SOC_PLATFORM=st-mp1 \
 	AQROOT=$(@D) \
 	DEBUG=0
 
@@ -47,15 +47,16 @@ GCNANO_BINARIES_USERLAND_SUBDIR = gcnano-userland-multi-$(GCNANO_BINARIES_USERLA
 define GCNANO_BINARIES_INSTALL
 	cd $(@D)/$(GCNANO_BINARIES_USERLAND_SUBDIR)/release/drivers/ ; \
 	find . -type f -exec $(INSTALL) -D -m 0755 {} $(1)/usr/lib/{} \; ; \
-	find . -type l -exec cp -a {} $(1)/usr/lib \;
+	for sharelib in *.so.*; do \
+		dev_lib_name=`echo $${sharelib} | awk -F'.so' '{print $$1}'`.so ; \
+		link_name=`$(TARGET_OBJDUMP) -x $${sharelib} 2>/dev/null | grep SONAME | sed 's/.* //'` ; \
+		ln -sf $${sharelib} $(1)/usr/lib/$${dev_lib_name} ; \
+		ln -sf $${sharelib} $(1)/usr/lib/$${link_name} ; \
+	done
 	mkdir -p $(1)/usr/include
 	cp -a $(@D)/$(GCNANO_BINARIES_USERLAND_SUBDIR)/release/include/* $(1)/usr/include/
-	ln -sf gbm/gbm.h $(1)/usr/include/gbm.h
-	cd $(@D)/$(GCNANO_BINARIES_USERLAND_SUBDIR)/pkgconfig/ ; \
-	for file in *.pc ; do \
-		sed -e "s|#PREFIX#|/usr|" $$file > $$file.temp ; \
-		$(INSTALL) -D -m 0644 $$file.temp $(1)/usr/lib/pkgconfig/$$file ; \
-	done
+	mkdir -p $(1)/usr/lib/pkgconfig/
+	cp -a $(@D)/$(GCNANO_BINARIES_USERLAND_SUBDIR)/pkgconfig/* $(1)/usr/lib/pkgconfig/
 endef
 
 define GCNANO_BINARIES_INSTALL_TARGET_CMDS
