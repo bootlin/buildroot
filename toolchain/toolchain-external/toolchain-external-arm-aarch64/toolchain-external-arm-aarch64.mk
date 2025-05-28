dc0be0975550ce28e553c3e239a13858278c9026
Kory Maincent <kory.maincent@bootlin.com>
2025-05-28 13:36:26 +0000

    toolchain-external-arm*: Add support for license file description
    Add support for ARM toolchain license description using the
    license.txt file as the license metadata. This ensures license compliance
    when using external ARM toolchains.
    
    Signed-off-by: Kory Maincent <kory.maincent@bootlin.com>

--- toolchain/toolchain-external/toolchain-external-arm-aarch64/toolchain-external-arm-aarch64.mk
+++ toolchain/toolchain-external/toolchain-external-arm-aarch64/toolchain-external-arm-aarch64.mk
@@ -8,5 +8,7 @@ TOOLCHAIN_EXTERNAL_ARM_AARCH64_VERSION = 13.2.rel1
 TOOLCHAIN_EXTERNAL_ARM_AARCH64_SITE = https://developer.arm.com/-/media/Files/downloads/gnu/$(TOOLCHAIN_EXTERNAL_ARM_AARCH64_VERSION)/binrel
 
 TOOLCHAIN_EXTERNAL_ARM_AARCH64_SOURCE = arm-gnu-toolchain-$(TOOLCHAIN_EXTERNAL_ARM_AARCH64_VERSION)-x86_64-aarch64-none-linux-gnu.tar.xz
+TOOLCHAIN_EXTERNAL_ARM_AARCH64_LICENSE = multiple (license.txt)
+TOOLCHAIN_EXTERNAL_ARM_AARCH64_LICENSE_FILES = license.txt
 
 $(eval $(toolchain-external-package))
